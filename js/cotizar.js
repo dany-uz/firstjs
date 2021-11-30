545f94e8b184213fefc6d23b604cd36de273c003
Daniel2204-slyr <cardaos2204@gmail.com>
2021-11-29 20:58:07 -0500

    Terminado 29/11/2021

--- js/cotizar.js
+++ js/cotizar.js
@@ -1,26 +1,29 @@
 function cotizar() {
 
-    let cantds = document.getElementById("preciosoftware").value;
-    let cantai = document.getElementById("preciointerfaz").value;
-    let cantst = document.getElementById("preciosoporte").value;
+    let cantds = document.getElementById('preciosoftware').value;
+    let cantai = document.getElementById('preciointerfaz').value;
+    let cantst = document.getElementById('preciosoporte').value;
 
     cs = cantds + cantai + cantst;
-    ctds = cantds*3500000;
+    ctds = cantds*3500000
     ctai = cantai*1500000;
     ctst = cantst*2000000;
     ct = ((cantds*3500000)+(cantai*1500000)+(cantst*2000000));
 
     if ((parseInt(cs)>5) && (parseInt(ct)>10000000)) {
+        st = ct
         vt = ct-(ct*0.25);
-        alert ('Usted deberá pagar $'+ vt + ' (25% de Desc. Incluido)');
-        alert ('Costo total por Diseño de Software: $'+ ctds);
-        alert ('Costo total por Actualización de Interfaz: $'+ ctai);
-        alert ('Costo total por Soporte Técnico: $'+ ctst);
     } else {
+        st = ct
         vt = ct
-        alert ('Usted deberá pagar $'+ vt);
-        alert ('Costo total por Diseño de Software: $'+ ctds);
-        alert ('Costo total por Actualización de Interfaz: $'+ ctai);
-        alert ('Costo total por Soporte Técnico: $'+ ctst);
-    }   
+    }
+    
+    document.getElementById('st').innerHTML = st;
+    document.getElementById('vt').innerHTML = vt;
+    document.getElementById('cantds').innerHTML = cantds ;
+    document.getElementById('cantai').innerHTML = cantai;
+    document.getElementById('cantst').innerHTML = cantst;
+    document.getElementById('ctds').innerHTML = ctds;
+    document.getElementById('ctai').innerHTML = ctai;
+    document.getElementById('ctst').innerHTML = ctst;
 }
